4a1c7483d7c3f35ae9badf8ed7315613de31d071
mcsherrylabs <alan@mcsherrylabs.com>
2021-06-15 16:32:45 +0100

    Add common protobuf to updatelist

--- updateProtos.sh
+++ updateProtos.sh
@@ -15,3 +15,10 @@ UNIQUE_TAG="storeandhash-service"
 TARGET_PATH="protos/store-and-hash-service/protobuf"
 
 ./mergeToSubfolderFromRemoteSubfolder.sh ${EXTERNAL_REPO} ${EXTERNAL_REPO_BRANCH} ${UNIQUE_TAG} ${EXTERNAL_REPO_PATH} ${TARGET_BRANCH} ${TARGET_PATH}
+
+
+EXTERNAL_REPO_PATH="common-service/src/main/protobuf"
+UNIQUE_TAG="common-service"
+TARGET_PATH="protos/common-service/protobuf"
+
+./mergeToSubfolderFromRemoteSubfolder.sh ${EXTERNAL_REPO} ${EXTERNAL_REPO_BRANCH} ${UNIQUE_TAG} ${EXTERNAL_REPO_PATH} ${TARGET_BRANCH} ${TARGET_PATH}
